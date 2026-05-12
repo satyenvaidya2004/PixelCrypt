fcb5db1f3b9403fca9325f2ea364ed9786465b8f
Satyen Vaidya <satyenvaidya2004@gmail.com>
2026-05-12 19:09:25 +0530

    Update API base URL to production endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // src/utils/api.js
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+const baseURL = process.env.REACT_APP_API_URL || "https://pixelcrypt-backend.onrender.com";
 
 const api = axios.create({
   baseURL,
